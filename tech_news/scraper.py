d49d73b7e479a946067d0f6365b487f994a2aacd
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-13 15:47:59 -0300

    feat: fetch function and scrape function implemented

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,33 @@
-# Requisito 1
+import requests
+import time
+from parsel import Selector
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url,
+                                headers={"user-agent": "Fake user-agent"},
+                                timeout=3)
+
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    return response.text
 
 
-# Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css("h2.entry-title > a::attr(href)").getall()
 
 
-# Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
 
 
-# Requisito 4
 def scrape_news(html_content):
     """Seu código deve vir aqui"""
 
 
-# Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
